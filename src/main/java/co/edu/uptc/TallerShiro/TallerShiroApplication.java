b10a28156827b4451c8872b62d177c93607daafe
Edwin Martinez <edwin.martinez04@uptc.edu.co>
2025-11-17 14:49:38 -0500

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/co/edu/uptc/TallerShiro/TallerShiroApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.TallerShiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerShiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uptc/TallerShiro/TallerShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.TallerShiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerShiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
